574e61774b810ffc2aa9668a046ce0fbdfd9e0b3
maortiz17 <maos17@educa.madrid.org>
2025-09-22 16:20:37 +0200

    Primera subida

---
+++ UT01/src/es/maos17/cdr/programacion/ut01/ejemplos/Ejemplo02.java
@@ -0,0 +1,25 @@
+package es.maos17.cdr.programacion.ut01.ejemplos;
+
+public class Ejemplo02 {
+
+	public static void main(String[] args) {
+		
+		// Prueba con char vs String
+		
+		char letra = 'a';
+		
+		// char letra2 = "b"; <- char siempre con comillas simples ''
+		
+		String cadena = "Hola a todos";
+		
+		String saludo2 = new String("Hola de nuevo");
+		
+		System.out.println(cadena);
+		System.out.println(saludo2);
+		
+		// String letra2 = 'a'; <- String siempre con comillas dobles ""
+		
+
+	}
+
+}
