e3ad1fd8794d8fd99ffb72985dc75c46bd6ab6b4
maortiz17 <maos17@educa.madrid.org>
2025-09-22 16:37:02 +0200

    Primera versión

---
+++ UT01/src/es/maos17/cdr/programacion/ut01/ejemplos/Ejemplo04.java
@@ -0,0 +1,13 @@
+package es.maos17.cdr.programacion.ut01.ejemplos;
+
+public class Ejemplo04 {
+
+	final static int NUMERO_FIJO = 2;
+	
+	public static void main(String[] args) {
+
+		final int NUMERO_VIDAS = 3;
+		
+		// NUMERO_VIDAS = 5; <- Esto está prohibido
+	}
+}
