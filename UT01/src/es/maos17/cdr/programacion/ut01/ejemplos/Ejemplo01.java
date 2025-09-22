88943f367e3644cd195c55d57a6853e17a124d10
maortiz17 <maos17@educa.madrid.org>
2025-09-22 16:14:05 +0200

    Primer programa

---
+++ UT01/src/es/maos17/cdr/programacion/ut01/ejemplos/Ejemplo01.java
@@ -0,0 +1,18 @@
+package es.maos17.cdr.programacion.ut01.ejemplos;
+
+public class Ejemplo01 {
+
+	public static void main(String[] args) {
+		// Aquí va el código de mi programa
+
+		// int numeroEntero = 20L; <- No puedo guardar un long en un int
+		
+		int numeroEntero = 0;
+		
+		numeroEntero = 20;
+		
+		System.out.println(numeroEntero);
+
+	}
+
+}
