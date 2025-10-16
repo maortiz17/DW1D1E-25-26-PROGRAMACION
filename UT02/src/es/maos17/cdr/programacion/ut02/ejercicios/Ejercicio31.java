348af596f20e31df8f57f78596b41329262a3bef
maortiz17 <maos17@educa.madrid.org>
2025-10-16 10:15:55 +0200

    comentarios adicionales

--- UT02/src/es/maos17/cdr/programacion/ut02/ejercicios/Ejercicio31.java
+++ UT02/src/es/maos17/cdr/programacion/ut02/ejercicios/Ejercicio31.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Ejercicio31 {
 	
+	// Constantes con número máximo a generar y número máximo de intentos
 	static final int NUMERO_MAXIMO = 99;
 	static final int MAX_INTENTOS = 10;
 	
@@ -23,20 +24,23 @@ public class Ejercicio31 {
 		
 		
 		Scanner sc = new Scanner(System.in);
-		Random rnd = new Random();
+		Random rnd = new Random(); // Objeto necesario para número aleatorio
 		
+		// El método genera un número entre cero (incluido) y NUMERO_MAXIMO (excluido)
+		// Como en nuestro caso es entre 1 y NUMERO_MAXIMO sumamos 1 
 		int numero = rnd.nextInt(NUMERO_MAXIMO) + 1;
 		int numLeido;
 		int intentos = 0;
-		boolean hasAcertado = false;
+		boolean hasAcertado = false; // Flag por si se acierta el número
 		
+		// Iteramos mientras tengamos intentos disponibles y no se haya acertado el número
 		while (intentos < MAX_INTENTOS && !hasAcertado) {
 			System.out.println("Te quedan " + (MAX_INTENTOS - intentos) + " intentos.");
 			intentos++;
 			System.out.print("Introduce un número: ");
 			numLeido = sc.nextInt();
 			
-			hasAcertado = (numero == numLeido);
+			hasAcertado = (numero == numLeido); // Ponemos el flag a true si se acierta el número
 			if (!hasAcertado) {
 				if (numero < numLeido) {
 					System.out.println("El número buscado es menor");
@@ -45,7 +49,7 @@ public class Ejercicio31 {
 				}
 			}
 		}
-		if (hasAcertado) {
+		if (hasAcertado) { // Mensajes al final del juego
 			System.out.println("Has acertado en " + intentos + " intentos.");
 		}else {
 			System.out.println("El número buscado era: " + numero);
